3f6ab8ab45e174583b492ec22ceda06caf2e559a
2819968842@qq.com <2819968842@qq.com>
2020-12-05 20:34:09 +0800

    element-ui学习

---
+++ element-ui-test/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <el-button @click="show">按钮</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  methods: {
+    show() {
+      this.$message('element-ui提示')
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
